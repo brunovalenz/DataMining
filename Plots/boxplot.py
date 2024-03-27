b560b3f006ecbc506128ba39aaafae266b894c8d
brunovalenz <brunovalenz@gmail.com>
2024-03-26 21:10:24 -0300

    Brian_god

--- Plots/boxplot.py
+++ Plots/boxplot.py
@@ -2,8 +2,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-# Dados de exemplo (substitua isso pelos seus próprios dados)
-
 names = ['N_Days','Status','Drug','Age','Sex','Ascites','Hepatomegaly','Spiders','Edema','Bilirubin','Cholesterol','Albumin','Copper','Alk_Phos','SGOT','Tryglicerides','Platelets','Prothrombin','Stage'] 
 features = ['N_Days', 'Status', 'Drug','Age','Sex','Ascites','Hepatomegaly','Spiders','Edema','Bilirubin','Cholesterol','Albumin','Copper','Alk_Phos','SGOT','Tryglicerides','Platelets','Prothrombin','Stage']
 input_file = '0-Datasets/cirrhosis2Clear.csv'
@@ -15,7 +13,7 @@ dados = pd.read_csv(input_file,         # Nome do arquivo com dados
 
 # Criar o boxplot
 plt.figure(figsize=(8, 6))
-plt.boxplot(dados)
+plt.boxplot(dados['Age'])
 
 # Adicionar título e rótulos aos eixos
 plt.title('Boxplot')
