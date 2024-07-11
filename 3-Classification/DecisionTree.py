e93c98517ea4e05c93f9c313f6cd68ffc7e9b494
brunovalenz <brunovalenz@gmail.com>
2024-07-11 11:13:30 -0300

    tree check

---
+++ 3-Classification/DecisionTree.py
@@ -0,0 +1,50 @@
+from matplotlib import pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.datasets import load_iris
+from sklearn.tree import DecisionTreeClassifier
+from sklearn import tree
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+
+def main():
+    
+    names = ['N_Days','Status','Drug','Age','Sex','Ascites','Hepatomegaly','Spiders','Edema','Bilirubin','Cholesterol','Albumin','Copper','Alk_Phos','SGOT','Tryglicerides','Platelets','Prothrombin','Stage'] 
+    features = ['N_Days', 'Status', 'Drug','Age','Sex','Ascites','Hepatomegaly','Spiders','Edema','Bilirubin','Cholesterol','Albumin','Copper','Alk_Phos','SGOT','Tryglicerides','Platelets','Prothrombin','Stage']
+    input_file = '0-Datasets/cirrhosis2Clear.csv'
+    target = 'Status'
+    df = pd.read_csv(input_file,         # Nome do arquivo com dados
+                        sep = ",",
+                        names = names,      # Nome das colunas 
+                        usecols = features, # Define as colunas que serão  utilizadas
+                        na_values='NA')      # Define que NA será considerado valores ausentes
+                   
+   
+    # Separating out the features
+    X = df.loc[:, features].values
+    print(X.shape)
+
+    # Separating out the target
+    y = df.loc[:,[target]].values
+
+    # Standardizing the features
+    X = StandardScaler().fit_transform(X)
+    
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=0)
+    print(X_train.shape)
+    print(X_test.shape)
+
+    clf = DecisionTreeClassifier(max_leaf_nodes=6)
+    clf.fit(X_train, y_train)
+    tree.plot_tree(clf)
+    plt.show()
+    
+    predictions = clf.predict(X_test)
+    print(predictions)
+    
+    result = clf.score(X_test, y_test)
+    print('Acuraccy:')
+    print(result)
+
+
+if __name__ == "__main__":
+    main()
