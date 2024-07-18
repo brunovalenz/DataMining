00d3c642c9f833549e2c5064d927894cbcf6d18e
brunovalenz <brunovalenz@gmail.com>
2024-07-18 10:43:43 -0300

    MENSAGEM DE COMMIT

--- 3-Classification/DecisionTree.py
+++ 3-Classification/DecisionTree.py
@@ -29,7 +29,7 @@ def main():
     # Standardizing the features
     X = StandardScaler().fit_transform(X)
     
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=0)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
     print(X_train.shape)
     print(X_test.shape)
 
