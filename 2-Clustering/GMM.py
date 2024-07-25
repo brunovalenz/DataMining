af788e90319a83218c3b5d8fdcd739812e6c4264
brianashihara <brian09.c.a@gmail.com>
2024-07-25 08:48:26 -0300

    arrumando alguns erros

--- 2-Clustering/GMM.py
+++ 2-Clustering/GMM.py
@@ -1,32 +1,29 @@
-import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
 from sklearn.mixture import GaussianMixture
 import pandas as pd
-import matplotlib.pyplot as plt
 
-# Dados normalizados
+# Suponha que você tenha um DataFrame chamado df com os dados normalizados
 
+# Para este exemplo, vamos usar uma base de dados fictícia normalizada
 names = ['N_Days','Status','Drug','Age','Sex','Ascites','Hepatomegaly','Spiders','Edema','Bilirubin','Cholesterol','Albumin','Copper','Alk_Phos','SGOT','Tryglicerides','Platelets','Prothrombin','Stage'] 
 features = ['N_Days', 'Status', 'Drug','Age','Sex','Ascites','Hepatomegaly','Spiders','Edema','Bilirubin','Cholesterol','Albumin','Copper','Alk_Phos','SGOT','Tryglicerides','Platelets','Prothrombin','Stage']
 input_file = '0-Datasets/cirrhosis2Clear.csv'
-X = pd.read_csv(input_file,         # Nome do arquivo com dados
+df_normalized  = pd.read_csv(input_file,         # Nome do arquivo com dados
                      sep = ",",
                      names = names,      # Nome das colunas 
                      usecols = features, # Define as colunas que serão  utilizadas
-                     na_values='NA')      # Define que NA será considerado valores ausentes
+                     na_values='NA')
 
-# Criando o modelo GMM
-gmm = GaussianMixture(n_components=3)  # Defina o número de componentes desejado
+# Definir o número de clusters
+num_clusters = 3
 
-# Treinando o modelo
-gmm.fit(X)
+# Aplicar o GMM
+gmm = GaussianMixture(n_components=num_clusters, random_state=42)
+df_normalized['cluster'] = gmm.fit_predict(df_normalized)
 
-# Prevendo as classes dos dados
-labels = gmm.predict(X)
-
-# Obtendo as probabilidades de pertencimento a cada classe
-probs = gmm.predict_proba(X)
-
-# Plotando os resultados
-plt.scatter(X[:, 0], X[:, 1], c=labels, cmap='viridis')
-plt.colorbar()
+# Visualizar os resultados com Seaborn
+plt.figure(figsize=(10, 6))
+sns.scatterplot(x='Age', y='Bilirubin', hue='cluster', palette='viridis', data=df_normalized, s=100)
+plt.title('GMM Clustering')
 plt.show()
